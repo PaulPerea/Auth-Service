283db29934e0101d3c8e061dfc00ef7fd0b42725
PaulPerea <pc474933@gmail.com>
2025-06-05 00:27:52 -0500

    aqui commit de master

---
+++ Users/ACER/OneDrive-CIBERTEC/Documents/jwt/proyect-jwt-sql/src/main/java/com/cibertec/entity/User.java
@@ -0,0 +1,35 @@
+package com.cibertec.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    private boolean enabled = true;
+
+    @ManyToMany(fetch = FetchType.EAGER) // Cargar roles inmediatamente
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
